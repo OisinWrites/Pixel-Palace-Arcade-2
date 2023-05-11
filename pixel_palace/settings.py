e578d79e64519825aaac8a43464f93854607af6b
OisinWrites <oisin.banville@gmail.com>
2023-05-11 19:19:00 +0000

    Add settings for allauth, link allauth templates to base.html

--- pixel_palace/settings.py
+++ pixel_palace/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'user_profile',
 ]
 
+SITE_ID = 1
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -48,6 +50,14 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'pixel_palace.urls'
+
+AUTHENTICATION_BACKENDS = [
+# Needed to login by username in Django admin, regardless of `allauth`
+'django.contrib.auth.backends.ModelBackend',
+
+# `allauth` specific authentication methods, such as login by e-mail
+'allauth.account.auth_backends.AuthenticationBackend',
+]
 
 TEMPLATES = [
     {

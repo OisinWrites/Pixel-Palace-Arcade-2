43d3642608fd72aa736f9f52e577dbbc04e2df9c
OisinWrites <oisin.banville@gmail.com>
2023-05-11 19:46:00 +0000

    Create admin display for customer model

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = (
+        'user',
+        'name',
+        'email',
+        'shipping_address',
+        'billing_address',
+        'nickname',
+    )
+
+
+admin.site.register(Customer, CustomerAdmin)
